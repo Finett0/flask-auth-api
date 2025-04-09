d7ce8ac052eadb831f9c81796f97c38cb7e445ca
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-04-09 20:47:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 @app.route('/hello-word',methods=['GET'])
 def hello_world():
