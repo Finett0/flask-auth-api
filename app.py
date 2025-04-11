244c67e40a1dde4cbb2b58b25652e22fafe428b9
Giovanni Finetto <finettogs@gmail.com>
2025-04-11 15:08:08 -0300

    Adicionando o primeiro endpoint

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+
+@app.route('/login',methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return jsonify({"message":"Autenicação realizda com sucesso"})
+
+    return jsonify ({"message":"Credenciais inválidas"}),404
+
 
 
 @app.route('/hello-word',methods=['GET'])
